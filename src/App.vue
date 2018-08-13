49386abb7e20affe09fec03255f4bf83db6aee0c
Shubham Bhadani <shubham.bhadani07@gmail.com>
2018-08-13 11:53:28 +0530

    Added Compenents

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <div>
-    <SearchBar></SearchBar>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+const API_KEY='AIzaSyAnAi9xKNqI_xNGDKHtFZrInz5l_QkMqNs';
 
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data(){
+    return { videos:[] };
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+      .get('https://www.googleapis.com/youtube/v3/search',{
+        params: {
+          key: API_KEY,
+          type:'video',
+          part:'snippet',
+          q: searchTerm
+        }
+      })
+      .then(response => {
+        this.videos = response.data.items;
+      });
+    }
   }
 };
 </script>
